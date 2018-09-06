f5187aa514a0ff0cc719001e256cc4bb2260ba55
Natalia Dulko <ms.duna@gmail.com>
2018-09-06 19:20:44 +0300

    Update PairedTag.js

--- JS/js_prototypes/PairedTag.js
+++ JS/js_prototypes/PairedTag.js
@@ -1,23 +1,17 @@
-//Конструктор, который принимает на вход: name, attributes, body, children.
-//Метод toString, который возвращает текстовое представление узла (html) на всю глубину.
-
 // BEGIN (write your solution here)
-export default class {
-  constructor(name, attributes = {}, body = '', children = []) {
-    this.name = name;
-    this.attributes = attributes;
+import Node from './Node';
+
+export default class extends Node {
+  constructor(name, attributes, body = '', children = []) {
+    super(name, attributes);
     this.body = body;
     this.children = children;
   }
 
   toString() {
-    const value = this.children.length > 0 ? this.children.join('') : this.body;
+    const value = this.children.length > 0 ?
+      this.children.join('') : this.body;
     return `<${this.name}${this.getAttributesAsLine()}>${value}</${this.name}>`;
-  }
-
-  getAttributesAsLine() {
-    return Object.keys(this.attributes).reduce((acc, key) =>
-      `${acc} ${key}="${this.attributes[key]}"`, '');
   }
 }
 // END
