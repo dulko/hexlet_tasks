d438d7e503b1810a144146240d5ebf35447edbd2
Natalia Dulko <ms.duna@gmail.com>
2018-09-06 19:20:58 +0300

    Update SingleTag.js

--- JS/js_prototypes/SingleTag.js
+++ JS/js_prototypes/SingleTag.js
@@ -1,20 +1,9 @@
-//Конструктор, который принимает на вход: name, attributes
-//Метод toString, который возвращает текстовое представление узла (html) на всю глубину.
-
 // BEGIN (write your solution here)
-export default class {
-  constructor(name, attributes = {}) {
-    this.name = name;
-    this.attributes = attributes;
-  }
+import Node from './Node';
 
+export default class extends Node {
   toString() {
     return `<${this.name}${this.getAttributesAsLine()}>`;
-  }
-
-  getAttributesAsLine() {
-    return Object.keys(this.attributes).reduce((acc, key) =>
-      `${acc} ${key}="${this.attributes[key]}"`, '');
   }
 }
 // END
