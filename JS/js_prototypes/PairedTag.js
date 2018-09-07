ede8d05b6658debfb6cbfb74d8fff97a1c6c3fa7
Natalia Dulko <ms.duna@gmail.com>
2018-09-07 11:30:17 +0300

    Update PairedTag.js

--- JS/js_prototypes/PairedTag.js
+++ JS/js_prototypes/PairedTag.js
@@ -1,17 +1,15 @@
 // BEGIN (write your solution here)
 import Node from './Node';
 
-export default class extends Node {
-  constructor(name, attributes, body = '', children = []) {
-    super(name, attributes);
-    this.body = body;
-    this.children = children;
-  }
-
-  toString() {
-    const value = this.children.length > 0 ?
-      this.children.join('') : this.body;
-    return `<${this.name}${this.getAttributesAsLine()}>${value}</${this.name}>`;
-  }
+export default function PairedTag(name, attributes = {}, body = '', children = []) {
+  Node.apply(this, [name, attributes]);
+  this.body = body;
+  this.children = children;
 }
+
+PairedTag.prototype.toString = function toString() {
+  const value = this.children.length > 0 ?
+    this.children.map(child => child.toString()).join('') : this.body;
+  return `<${this.name}${this.getAttributesAsLine()}>${value}</${this.name}>`;
+};
 // END
