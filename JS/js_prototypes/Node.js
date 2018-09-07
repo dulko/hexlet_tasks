d6f451fd2e7f855318ba90759991032a2e9f1aee
Natalia Dulko <ms.duna@gmail.com>
2018-09-07 11:39:23 +0300

    Update Node.js

--- JS/js_prototypes/Node.js
+++ JS/js_prototypes/Node.js
@@ -1,13 +1,11 @@
 // BEGIN (write your solution here)
-export default class {
-  constructor(name, attributes = {}) {
-    this.name = name;
-    this.attributes = attributes;
-  }
-
-  getAttributesAsLine() {
-    return Object.keys(this.attributes).reduce((acc, key) =>
-      `${acc} ${key}="${this.attributes[key]}"`, '');
-  }
+export default function Node(name, attributes = {}) {
+  this.name = name;
+  this.attributes = attributes;
 }
+
+Node.prototype.getAttributesAsLine = function getAttributesAsLine() {
+  return Object.keys(this.attributes).reduce((acc, key) =>
+    `${acc} ${key}="${this.attributes[key]}"`, '');
+};
 // END
