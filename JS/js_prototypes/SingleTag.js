2cb3bfe7f8e6beabb3e7750b0cf4e431794e6f2d
Natalia Dulko <ms.duna@gmail.com>
2018-09-07 11:29:59 +0300

    Update SingleTag.js

--- JS/js_prototypes/SingleTag.js
+++ JS/js_prototypes/SingleTag.js
@@ -1,9 +1,11 @@
 // BEGIN (write your solution here)
 import Node from './Node';
 
-export default class extends Node {
-  toString() {
-    return `<${this.name}${this.getAttributesAsLine()}>`;
-  }
+export default function SingleTag(name, attributes = {}) {
+  Node.apply(this, [name, attributes]);
 }
+
+SingleTag.prototype.toString = function toString() {
+  return `<${this.name}${this.getAttributesAsLine()}>`;
+};
 // END
